8af59460ffe29bbf0870f318d774f1d2e183cb47
sunShineLoveMe <juneandrao@outlook.com>
2025-12-12 10:55:48 +0800

    更新网站的icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -79,21 +79,8 @@ export const metadata: Metadata = {
   
   // Icons
   icons: {
-    icon: [
-      {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
-        media: "(prefers-color-scheme: dark)",
-      },
-      {
-        url: "/icon.svg",
-        type: "image/svg+xml",
-      },
-    ],
-    apple: "/apple-icon.png",
+    icon: "/logo-icon.png",
+    apple: "/logo-icon.png",
   },
   
   // Other
